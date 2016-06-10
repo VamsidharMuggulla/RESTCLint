2aa2e7eeeb97c3f91cd33b2e1d81c32039f58b7b
Vamsidhar Muggulla <vam.vamshe@gmail.com>
2016-06-11 00:05:14 +0530

    Update newwindow.js

--- REST Clint/newwindow.js
+++ REST Clint/newwindow.js
@@ -1,5 +1,10 @@
-opennewtab() {
-  var win = window.open("http://www.fb.com", '_blank');
-  win.focus();
+document.addEventListener('DOMContentLoaded', function() {
+start=document.getElementById("newtab");
+start.onclick=function() {
+chrome.tabs.create({url : 'popup.html'}, function(tab) { 
+    //chrome.tabs.executeScript(tab.id, {file: 'src/contentScript2'});
+  });
 }
+});
+
 
