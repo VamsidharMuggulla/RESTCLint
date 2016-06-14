fa683e6bd4390c1ddf00d89a83ece45dad85144e
Vamsidhar Muggulla <vam.vamshe@gmail.com>
2016-06-14 16:14:14 +0530

    Update newwindow.js

--- REST Clint/newwindow.js
+++ REST Clint/newwindow.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
 start=document.getElementById("newtab");
 start.onclick=function() {
-chrome.tabs.create({url : 'home.html'}, function(tab) { 
+chrome.tabs.create({url : 'http://localhost:8080/RestClint/'}, function(tab) { 
     //chrome.tabs.executeScript(tab.id, {file: 'src/contentScript2'});
   });
 }
